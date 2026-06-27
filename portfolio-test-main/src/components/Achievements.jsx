5962ccc58212f8329c2cb025928b78d468c6022e
Mohamed Younus <mohamedyounus0572@gmail.com>
2026-06-27 12:53:57 +0530

    Update heading font size using clamp function

--- portfolio-test-main/src/components/Achievements.jsx
+++ portfolio-test-main/src/components/Achievements.jsx
@@ -26,7 +26,7 @@ const Achievements = () => {
         
         {/* Section Heading */}
         <div className="flex flex-col gap-4">
-       <h2 className="text-[2rem] sm:text-5xl md:text-6xl font-extrabold tracking-tight leading-none font-syne">
+      <h2 className="text-[clamp(1.6rem,7vw,4rem)] font-extrabold tracking-tight leading-none font-syne">
   ACHIEVEMENTS
 </h2>
         </div>
